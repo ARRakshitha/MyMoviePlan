f47d749bf94085a21e66ba863b090d5ec4c992ea
ARRakshitha <rakshithaar878@gmail.com>
2023-04-01 16:37:17 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { MovieFormComponent } from './movie-form/movie-form.component';
+import { MovieComponent } from './movie/movie.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MyBookingsComponent } from './my-bookings/my-bookings.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    MovieFormComponent,
+    MovieComponent,
+    MovieListComponent,
+    MoviesComponent,
+    MyBookingsComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
